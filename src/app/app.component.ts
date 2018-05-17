5452f9dc125c060f9e602a871d878689871c40cc
Luiz <argentinaluiz@gmail.com>
2018-05-17 15:30:38 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import {MyServiceService} from './my-service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'School of Net';
+
+  nome = '';
+
+  constructor(private myService:MyServiceService){
+    console.log(myService.variavel);
+  }
+
+  //Angular Elements - Web components agnostico/desacoplados ****************** Vue.js, React.js, Ember.js, Backbone.js
+
+  //jquery - jquery upload - bootstrap - outras coisas
+
+  //tag personalizada - vida propria <upload></upload>
 }
+
+
+//Ivy Renderer - Renderização de templates de forma otimizada
+
+//Bazel
+
+//Angular Elements
+
+//Angular Material - chega na aplicação e cria uma área de grid
+
+//TypeScript 2.7
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Injector} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {MessageComponent} from './message/message.component';
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MessageComponent
+    ],
+    entryComponents: [
+        MessageComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(private injector: Injector) {
+        const customElement = createCustomElement(MessageComponent, {injector});
+        customElements.define('my-message', customElement);
+        //Web components
+    }
+
+    ngDoBootstrap() {
+    }
+}
+//ng g component
